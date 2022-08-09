c8978e6b0d7c6123b02003b647965bb72113150e
zito-s <jhhj424@naver.com>
2022-08-09 14:47:05 +0900

    test: 재고 감소 - 동시성 문제 해결 5
    redis lettuce 사용
    
    - 구현이 간단하다
    - 네임드락이랑 비슷하지만 세션 관리를 하지 않아도됨
    - spin lock 방식이라 redis 에 부하를 줄 수 있음

---
+++ src/main/java/com/example/stockexample/facade/LettuceLockStockFacade.java
@@ -0,0 +1,29 @@
+package com.example.stockexample.facade;
+
+import com.example.stockexample.repository.RedisLockRepository;
+import com.example.stockexample.service.StockService;
+import org.springframework.stereotype.Component;
+
+@Component
+public class LettuceLockStockFacade {
+
+    private final RedisLockRepository redisLockRepository;
+    private final StockService stockService;
+
+    public LettuceLockStockFacade(RedisLockRepository redisLockRepository, StockService stockService) {
+        this.redisLockRepository = redisLockRepository;
+        this.stockService = stockService;
+    }
+
+    public void decrease(Long key, Long quantity) throws InterruptedException {
+        while (!redisLockRepository.lock(key)) {
+            Thread.sleep(100);
+        }
+
+        try {
+            stockService.decrease(key, quantity);
+        } finally {
+            redisLockRepository.unlock(key);
+        }
+    }
+}
